b87685f534055addfc86fc714752eb4417a32ee3
husenaiah.g <husenaiah.gandham@kagamierp.com>
2022-03-01 19:14:44 +0530

    fileupload example in springbiit

--- Spring-Boot-Test/src/main/java/springboot/example/controller/DepartmentController.java
+++ Spring-Boot-Test/src/main/java/springboot/example/controller/DepartmentController.java
@@ -1,9 +1,13 @@
 package springboot.example.controller;
 
+import java.io.IOException;
 import java.util.List;
 //Importing required classes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import springboot.example.model.Department;
 import springboot.example.service.DepartmentService;
@@ -15,7 +19,7 @@ public class DepartmentController {
 	@Autowired
 	private DepartmentService departmentService;
 
-	// Save operation
+	// Save operation// SpringBootTest
 	@PostMapping("/departments")
 	public Department saveDepartment(@RequestBody Department department) {
 		return departmentService.saveDepartment(department);
@@ -39,6 +43,17 @@ public class DepartmentController {
 		departmentService.deleteDepartmentById(departmentId);
 
 		return "Deleted Successfully";
+	}
+	
+	
+	@PostMapping("/fileupload")
+	public ResponseEntity<Object> saveDepartment(@RequestParam("file") MultipartFile file) throws IOException {
+		
+		 byte[] fileContent=file.getBytes();
+	     System.out.println(file.getSize());
+	     System.out.println(file.getOriginalFilename());
+	     System.out.println(file.getName());
+		 return new ResponseEntity<Object>("file uploaded",HttpStatus.OK);
 	}
 }
 
